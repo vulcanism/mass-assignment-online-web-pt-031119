022ecba80a0877902f586b179a83644f9dff4463
Savannah C <sgcorrell@gmail.com>
2019-04-12 18:34:23 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize
+  def initialize(name:, birthday:)
   
   end
 end
